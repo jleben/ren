4717cf163717e752e49e72c4cc6bd577c8bd3f14
Jakob Leben <jakob.leben@gmail.com>
2017-01-27 11:46:39 -0800

    Add command line option to open file at start

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include "main_window.hpp"
 
 #include <QApplication>
+#include <iostream>
 
 using namespace std;
 using namespace datavis;
@@ -10,6 +11,16 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     auto main_win = new MainWindow;
+
+    auto args = app.arguments();
+
+    if (args.size() > 1)
+    {
+        auto file_path = args[1];
+        cout << "Opening file: " << file_path.toStdString() << endl;
+        main_win->openDataFile(file_path);
+    }
+
     main_win->resize(800,600);
     main_win->show();
 
